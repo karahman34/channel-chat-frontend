08f69342eee6969c17e0103bfeef0b2b96932161
karahman <jakarahman007@gmail.com>
2020-10-18 10:32:21 +0700

    clean up

--- src/App.vue
+++ src/App.vue
@@ -34,20 +34,14 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      Hello World
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
 
   data: () => ({
     //
