b86fc43a6c349d2b33ab77479dd34d4c40969b6a
karahman <jakarahman007@gmail.com>
2020-12-16 17:15:42 +0700

    conditional path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: "/channel-chat-frontend/"
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/channel-chat-frontend/" : "/"
 };
