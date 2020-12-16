ca06d9353f984b3ad040cefdca7ccc465f30ef19
karahman <jakarahman007@gmail.com>
2020-12-16 16:57:31 +0700

    gh-pages

---
+++ precache-manifest.659b86f15603e3c50e4b5580aef8c4a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc14e22949dbc3959fb8",
+    "url": "/channel-chat-frontend/css/app.46536328.css"
+  },
+  {
+    "revision": "95426e72840bb36993b7",
+    "url": "/channel-chat-frontend/css/chunk-12b5c994.fd09e771.css"
+  },
+  {
+    "revision": "3b79d7315f0c155bb165",
+    "url": "/channel-chat-frontend/css/chunk-52cc49da.ec23364c.css"
+  },
+  {
+    "revision": "306a8b03cba430e77852",
+    "url": "/channel-chat-frontend/css/chunk-5a64bd17.2353e9f4.css"
+  },
+  {
+    "revision": "2e376bde440f8e6b45ce",
+    "url": "/channel-chat-frontend/css/chunk-vendors.714530d1.css"
+  },
+  {
+    "revision": "c87858720c6aff009bce4c36922d289a",
+    "url": "/channel-chat-frontend/index.html"
+  },
+  {
+    "revision": "cc14e22949dbc3959fb8",
+    "url": "/channel-chat-frontend/js/app.fa3952b5.js"
+  },
+  {
+    "revision": "95426e72840bb36993b7",
+    "url": "/channel-chat-frontend/js/chunk-12b5c994.519f8417.js"
+  },
+  {
+    "revision": "3b79d7315f0c155bb165",
+    "url": "/channel-chat-frontend/js/chunk-52cc49da.150a7ebd.js"
+  },
+  {
+    "revision": "306a8b03cba430e77852",
+    "url": "/channel-chat-frontend/js/chunk-5a64bd17.16e1c699.js"
+  },
+  {
+    "revision": "2e376bde440f8e6b45ce",
+    "url": "/channel-chat-frontend/js/chunk-vendors.829829ac.js"
+  },
+  {
+    "revision": "a224424945aae13ec6d5ab13a844d543",
+    "url": "/channel-chat-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/channel-chat-frontend/robots.txt"
+  }
+]);
