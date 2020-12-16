8c149f333575f7a4629b8af38f3050b6a8c607ac
karahman <jakarahman007@gmail.com>
2020-12-16 16:02:07 +0700

    Minor fixes

--- src/main.js
+++ src/main.js
@@ -24,17 +24,19 @@ const initializeApp = () => {
 
   // Router guard
   router.beforeEach((to, from, next) => {
+    // Auth State
+    const { loggedIn } = store.state.auth;
+
+    // Middlewares
+    const { guest, auth } = to.meta;
+
+    if (guest === true && loggedIn) return router.push({ name: "Home" });
+    if (auth === true && !loggedIn) return router.push({ name: "Login" });
+
     // Set window title
     const title = to.meta?.title;
     const appTitle = process.env.VUE_APP_TITLE;
     document.title = title ? `${title} | ${appTitle}` : appTitle;
-
-    // Middlewares
-    const isLoggedIn = store.state.auth.loggedIn;
-    const { guest, auth } = to.meta;
-
-    if (guest === true && isLoggedIn) router.push({ name: "Home" });
-    if (auth === true && !isLoggedIn) router.push({ name: "Login" });
 
     // Next
     next();
@@ -54,20 +56,18 @@ const initializeApp = () => {
   // Set body loading
   document.querySelector("#app").innerHTML = "Loading...";
 
-  // Check token auth
+  // Check auth token
   const authToken = localStorage.getItem(tokenName);
-  if (!authToken) return initializeApp();
-
-  // check token expire
   let tokenExpiredDate = localStorage.getItem(expiredName);
-  if (!tokenExpiredDate) return initializeApp();
+
+  if (!tokenExpiredDate || !authToken) return initializeApp();
 
   const dateNow = new Date();
   tokenExpiredDate = new Date(tokenExpiredDate);
 
   if (dateNow > tokenExpiredDate) {
     removeToken();
-    initializeApp();
+    return initializeApp();
   }
 
   // Get user credentials
